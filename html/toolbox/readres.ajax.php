ad62ec1719ba871976f5847c81f1f4b9dfcb5780
yann <Mwyann@users.noreply.github.com>
2018-06-20 18:53:57 +0000

    Amélioration interface readres (sauvegarde en base)
    git-svn-id: https://svn.ydkjs.eu@222 474cd47a-f06f-4151-8730-396d9296b85e

--- html/toolbox/readres.ajax.php
+++ html/toolbox/readres.ajax.php
@@ -1,8 +1,8 @@
 <?php
 
-if ($_POST['type'] == 'animationList') {
-    require 'mysql.inc.php';
+require 'mysql.inc.php';
 
+if ($_POST['type'] == 'animationList') {
     $sp = explode('/',$_POST['animationFile']);
     $animationFile = intval($sp[sizeof($sp)-1]);
 
@@ -10,3 +10,23 @@ if ($_POST['type'] == 'animationList') {
     echo json_encode($res->fetchAll());
 }
 
+if ($_POST['type'] == 'saveAnimation') {
+    $sp = explode('/',$_POST['animationFile']);
+    $animationFile = intval($sp[sizeof($sp)-1]);
+    $framestart = intval($_POST['framestart']);
+    $framestop = intval($_POST['framestop']);
+    if ($framestop <= 0) $framestop = 'NULL';
+    $loopani = intval($_POST['loopani']);
+
+    $DB->query("INSERT INTO resani (grp, name, variantType, variantValue, resid, framestart, framestop, loopani) 
+                       VALUES (
+                        '".addslashes(trim($_POST['grp']))."',
+                        '".addslashes(trim($_POST['name']))."',
+                        '".addslashes(trim($_POST['variantType']))."',
+                        '".addslashes(trim($_POST['variantValue']))."',
+                        ".$animationFile.",
+                        ".$framestart.",
+                        ".$framestop.",
+                        ".$loopani.")");
+    echo json_encode(array('return' => 0));
+}
